d340fa8fee0e5be2ebfe3f5373509813c076bf5b
Rishabh Sahay <rishabhsahaya@gmail.com>
2025-05-26 23:37:43 +0530

    get all travel story api route

--- backend/controllers/travelStory.controller.js
+++ backend/controllers/travelStory.controller.js
@@ -34,3 +34,18 @@ export const addTravelStory = async(req, res, next) => {
         next(error)
     }
 }
+
+export const getAllTravelStory = async(req, res, next) => {
+    const userId = req.user.id
+
+    try {
+        const travelStories = await TravelStory.find({userId: userId}).sort({
+            isFavourite: -1
+        })
+
+        res.status(200).json({ stories: travelStories })
+        
+    } catch (error) {
+        next(error)
+    }
+}
